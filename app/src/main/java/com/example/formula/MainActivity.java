17de8a42f0179aaa381a3f943d8b246ffc478192
WerdnaLes <werdnales@gmail.com>
2022-08-25 17:50:03 +0300

    Deleted commented code

--- app/src/main/java/com/example/formula/MainActivity.java
+++ app/src/main/java/com/example/formula/MainActivity.java
@@ -37,33 +37,6 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 }
             }
-
-            // Old trash code:
-//            if (val1 == null) {
-//                temp = text.getText().toString();
-//                val1 = Float.parseFloat(temp);
-//                settings.put("R1", val1);
-//            } else if (val2 == null) {
-//                temp = text.getText().toString();
-//                val2 = Float.parseFloat(temp);
-//                settings.put("R2", val2);
-//            } else if (val3 == null) {
-//                temp = text.getText().toString();
-//                val3 = Float.parseFloat(temp);
-//                settings.put("R3", val3);
-//            } else if (val4 == null) {
-//                temp = text.getText().toString();
-//                val4 = Float.parseFloat(temp);
-//                settings.put("R4", val4);
-//            } else if (val5 == null) {
-//                temp = text.getText().toString();
-//                val5 = Float.parseFloat(temp);
-//                settings.put("R5", val5);
-//            } else if (val6 == null) {
-//                temp = text.getText().toString();
-//                val6 = Float.parseFloat(temp);
-//                settings.put("R6", val6);
-//            }
             stackView.setText(settings.toString());
             calcValue(settings.size());
         } catch (NumberFormatException e) {
@@ -93,29 +66,6 @@ public class MainActivity extends AppCompatActivity {
             result = equation.toString();
         }
         resultView.setText(result);
-
-        // Old trash code:
-//        else if (settings.size() == 2) {
-//            equation = 1 / ((1 / val1) + (1 / val2));
-//            result = equation.toString();
-//            resultView.setText(result);
-//        } else if (settings.size() == 3) {
-//            equation = 1 / ((1 / val1) + (1 / val2) + (1 / val3));
-//            result = equation.toString();
-//            resultView.setText(result);
-//        } else if (settings.size() == 4) {
-//            equation = 1 / ((1 / val1) + (1 / val2) + (1 / val3) + (1 / val4));
-//            result = equation.toString();
-//            resultView.setText(result);
-//        } else if (settings.size() == 5) {
-//            equation = 1 / ((1 / val1) + (1 / val2) + (1 / val3) + (1 / val4) + (1 / val5));
-//            result = equation.toString();
-//            resultView.setText(result);
-//        } else if (settings.size() == 6) {
-//            equation = 1 / ((1 / val1) + (1 / val2) + (1 / val3) + (1 / val4) + (1 / val5) + (1 / val6));
-//            result = equation.toString();
-//            resultView.setText(result);
-//        }
     }
 
 
